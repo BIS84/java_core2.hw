8b59aa8cbeddfc31ce02ff04dd1c4ef3ba7c8b66
BIS84 <bbiiss84@mail.ru>
2021-02-02 22:41:20 +0300

    add task2

--- src/HW2/Main.java
+++ src/HW2/Main.java
@@ -2,8 +2,9 @@ package HW2;
 
 public class Main {
 
-    public static void comparisonOfArrayLength (String[][] arr) throws MyArraySizeException {
+    public static int comparisonOfArrayLength (String[][] arr) throws MyArraySizeException {
 
+        int sum = 0;
         if (arr.length != 4) {
             throw new MyArraySizeException();
         }
@@ -13,8 +14,22 @@ public class Main {
             if (arr[i].length != 4) {
                 throw new MyArraySizeException();
             }
+
+
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    sum = sum + Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    System.out.println("В ячейке [" + i + "][" + j + "] данные отличные от Integer");
+                    throw new MyArrayDataException();
+                }
+            }
         }
+        return sum;
     }
+
+
+
 
     public static void main (String[] args) {
 
